1836e6ae165d8ca95ed32d16a8b019f2aeb89366
Talenserv Mac <talentservmac@Talenservs-Mac-mini.local>
2016-11-10 15:30:20 +0530

    commiting the notes for bluetooth low energy

---
+++ BLEPOCTalentserv/BLEPOCTalentserv/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BLEPOCTalentserv
+//
+//  Created by Talenserv Mac on 11/10/16.
+//  Copyright © 2016 Talenserv Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
